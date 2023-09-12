a9208e04c2cda900c5a4266ed85eb5d328d49b85
João Victor Maieru Teixeira <soljoaomaieru@hotmail.com>
2023-09-11 22:01:20 -0300

    Adicionado suite de teste

---
+++ TestSuite/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace TestSuite
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(false);
+        }
+    }
+}
