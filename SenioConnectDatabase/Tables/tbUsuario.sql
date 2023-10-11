097e15104b917969f5924126e2ab3afd824492f6
Maieru <soljoaomaieru@hotmail.com>
2023-10-11 20:27:36 -0300

    Adição de solução de banco de schema

---
+++ SenioConnectDatabase/Tables/tbUsuario.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tbUsuario]
+(
+	[Id]				INT				NOT NULL PRIMARY KEY IDENTITY (1, 1),
+	[Usuario]			VARCHAR(200)	NOT NULL,
+	[Senha]				VARCHAR(MAX)	NOT NULL,
+	[AssinaturaId]		INT				NOT NULL FOREIGN KEY REFERENCES dbo.tbAssinatura(Id)
+)
