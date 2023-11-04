c9c285e9053f6a8ee4fcfdb20baa4be914a11376
Maieru <soljoaomaieru@hotmail.com>
2023-11-04 19:28:26 -0300

    Criação de APIs de health da aplicação

---
+++ SeniorConnect/Controllers/HealthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Negocio.Database;
+
+namespace SeniorConnect.Controllers
+{
+    [ApiController]
+    [Route("Health/v1")]
+    public class HealthController : BaseController
+    {
+        public HealthController(ApplicationContext applicationContext) : base(applicationContext)
+        {
+        }
+
+        [HttpGet("WebsiteHealth")]
+        public async Task<IActionResult> WebsiteHealth()
+        {
+            return Ok("OK");
+        }
+
+        [HttpGet("DatabaseHealth")]
+        public async Task<IActionResult> DatabaseHealth()
+        {
+            try
+            {
+                var numeroAssinaturas = await ApplicationContext.Assinaturas.CountAsync();
+                return Ok("Ok");
+            }
+            catch
+            {
+                return Ok("Sem acesso ao banco de dados");
+            }
+        }
+    }
+}
