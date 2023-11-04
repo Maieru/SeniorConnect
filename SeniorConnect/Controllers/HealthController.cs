223fd032c440cc9d1f808173a41199d45a81f759
Maieru <soljoaomaieru@hotmail.com>
2023-11-04 19:42:37 -0300

    Adição de Allow Anonymous nas requisições do HealthController. Ressureição da HomePage

--- SeniorConnect/Controllers/HealthController.cs
+++ SeniorConnect/Controllers/HealthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Negocio.Database;
 
@@ -6,6 +7,7 @@ namespace SeniorConnect.Controllers
 {
     [ApiController]
     [Route("Health/v1")]
+    [AllowAnonymous]
     public class HealthController : BaseController
     {
         public HealthController(ApplicationContext applicationContext) : base(applicationContext)
