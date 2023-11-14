ceaedb96543c92972134c1101bdc659d0973504b
Maieru <soljoaomaieru@hotmail.com>
2023-11-14 19:21:00 -0300

    Adição de versão inicial de serviço de email

---
+++ Negocio/Helpers/EmailHelper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Helpers
+{
+    public static class EmailHelper
+    {
+        private static string ChaveSendGrid { get; set; }
+        private static string Remetente { get; set; }
+
+        public static async Task ConfiguraServico(SecretsHelper secretHelper)
+        {
+            ChaveSendGrid = await secretHelper.GetSendGridAPIKey();
+            Remetente = await secretHelper.GetRemetenteEmail();
+        }
+
+        public static async Task EnviaEmail(string assunto, string mensagem, string destinatario, string nomeDestinatario)
+        {
+            var client = new SendGridClient(ChaveSendGrid);
+            var remetente = new EmailAddress(Remetente, "SeniorConnect");
+            var emailDestinatario = new EmailAddress(destinatario, nomeDestinatario);
+            
+            var msg = MailHelper.CreateSingleEmail(remetente, emailDestinatario, assunto, mensagem, mensagem);
+            await client.SendEmailAsync(msg).ConfigureAwait(false);
+        }
+    }
+}
--- Negocio/Helpers/SecretsHelper.cs
+++ Negocio/Helpers/SecretsHelper.cs
@@ -14,13 +14,15 @@ namespace Negocio.Helpers
     {
         private string cachedWorkFactor;
         private string cachedEncryptionSalt;
-
+        
         private const string VAULT_NAME_ENVIROMENT = "KEY_VAULT_NAME";
 
         private const string MONGO_DB_CONNECTION_STRING_KEY = "MongoDbDatabase";
         private const string SQL_SERVER_CONNECTION_STRING_KEY = "SqlServerDatabase";
         private const string ENCRYPTION_WORK_FACTOR = "EncryptionWorkFactor";
         private const string ENCRYPTION_SALT = "EncryptionSalt";
+        private const string SEND_GRID_API_KEY = "SendGridApiKey";
+        private const string REMETENTE_EMAIL = "RemetenteEmail";
 
         private readonly string? ambiente;
 
@@ -85,6 +87,26 @@ namespace Negocio.Helpers
             cachedEncryptionSalt = secret.Value.Value;
 
             return cachedEncryptionSalt;
+        }
+
+        public async Task<string?> GetSendGridAPIKey()
+        {
+            if (ambiente == EnvironmentNames.AMBIENTE_LOCAL)
+                return Environment.GetEnvironmentVariable(SEND_GRID_API_KEY);
+
+            var secret = await SecretClient.GetSecretAsync(SEND_GRID_API_KEY);
+            
+            return secret.Value.Value;
+        }
+
+        public async Task<string?> GetRemetenteEmail()
+        {
+            if (ambiente == EnvironmentNames.AMBIENTE_LOCAL)
+                return Environment.GetEnvironmentVariable(REMETENTE_EMAIL);
+
+            var secret = await SecretClient.GetSecretAsync(REMETENTE_EMAIL);
+
+            return secret.Value.Value;
         }
 
         private string MontaUrlVault()
